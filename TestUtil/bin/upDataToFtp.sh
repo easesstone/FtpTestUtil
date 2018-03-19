b9d92abffe83df583ebae1e37c99a230493c2e12
sukidasakura <mashencai@126.com>
2018-03-19 17:04:35 +0800

    修改脚本，添加图片

--- TestUtil/bin/upDataToFtp.sh
+++ TestUtil/bin/upDataToFtp.sh
@@ -17,35 +17,10 @@ BIN_DIR=`pwd`    ### bin目录
 cd ..
 DEPLOY_DIR=`pwd`
 CONF_DIR=$DEPLOY_DIR/conf    ### 项目根目录
-SERVER_NAME=`sed '/ftpserver.application.name/!d;s/.*=//' conf/cluster-over-ftp.properties | tr -d '\r'` #获取服务名称
-SERVER_PORT=`sed '/listener-port/!d;s/.*=//' conf/cluster-over-ftp.properties | tr -d '\r'` #获取服务端口号
-
-cd ..
-declare -r BIGDATA_SERVICE_DIR=`pwd`
-declare -r COMMMON_DIR=${BIGDATA_SERVICE_DIR}/common
-declare -r FTP_DIR=${BIGDATA_SERVICE_DIR}/ftp
-declare -r SERVICE=${BIGDATA_SERVICE_DIR}/service
-declare -r CLUSTER_DIR=${BIGDATA_SERVICE_DIR}/cluster
-
-cd -
-COMMON_JARS=`ls ${COMMMON_DIR}/lib | grep .jar | awk '{print "'${COMMMON_DIR}/lib'/"$0}'|tr "\n" ":"`
-
-if [ -z "$SERVER_NAME" ]; then
-    SERVER_NAME=`hostname`
-fi
-
-
-if [ -n "$SERVER_PORT" ]; then
-    SERVER_PORT_COUNT=`netstat -tln | grep $SERVER_PORT | wc -l`
-    if [ $SERVER_PORT_COUNT -gt 0 ]; then
-        echo "ERROR: The $SERVER_NAME port $SERVER_PORT already used!"
-        exit 1
-    fi
-fi
 
 LIB_DIR=$DEPLOY_DIR/lib        ## Jar 包目录
 LIB_JARS=`ls $LIB_DIR|grep .jar|awk '{print "'$LIB_DIR'/"$0}'|tr "\n" ":"`    ## jar 包位置以及第三方依赖jar包，绝对路径
-LIB_JARS=${LIB_JARS}${COMMON_JARS}
+LIB_JARS=${LIB_JARS}
 LOG_DIR=${DEPLOY_DIR}/logs                       ## log 日记目录
 LOG_FILE=${LOG_DIR}/upDataToFtp.log        ##  log 日记文件
 
@@ -62,7 +37,7 @@ function upDataToFtp()
         mkdir $LOG_DIR;
     fi
     echo "up data to ftp start..."
-    nohup java -server -Xms2g -Xmx4g  -XX:PermSize=512m -XX:MaxPermSize=512m  -classpath $CONF_DIR:$LIB_JARS com.hzgc.updata.UpDataToFtp > ${LOG_FILE} 2>&1 &
+    nohup java -server -Xms2g -Xmx4g  -XX:PermSize=512m -XX:MaxPermSize=512m  -classpath $CONF_DIR:$LIB_JARS com.hzgc.test.updata.UpDataToFtp > ${LOG_FILE} 2>&1 &
  }
 
 #####################################################################
